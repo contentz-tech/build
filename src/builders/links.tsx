1dd17fbaa2e64a280f6d9592050ea871f877e998
Sergio Xalambrí <hello@sergiodxa.com>
2019-09-03 01:24:46 -0300

    Avoid building /links if not required

--- src/builders/links.tsx
+++ src/builders/links.tsx
@@ -3,13 +3,14 @@ import { jsx } from "@emotion/core";
 import { render } from "../utils/render";
 import { IState } from "../state";
 import { check } from "../utils/cache";
-import { writeFile, makeDir } from "../utils/fs";
+import { writeFile, makeDir, del } from "../utils/fs";
 
 import LinksPage from "../components/links";
 import Document from "../components/document";
 import { resolve } from "path";
 
 async function builder(state: IState) {
+  if (state.links.length === 0) return await del("public/links");
   if (
     (await check("config.yml", state.config)) &&
     (await check("links.yml", JSON.stringify(state.links)))
