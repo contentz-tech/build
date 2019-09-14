5d2cfd5dac3a59d84b1e03db14e15ddcf625a1cc
Sergio Xalambrí <hello@sergiodxa.com>
2019-09-14 20:57:56 -0300

    Add Card test

---
+++ src/components/card.test.tsx
@@ -0,0 +1,29 @@
+/** * @jest-environment jsdom */
+import '@testing-library/jest-dom/extend-expect'
+import { jsx } from "@emotion/core";
+import { render } from "@testing-library/react";
+import Card from "./card";
+
+describe("Card", () => {
+  test("Non Selectable", () => {
+    const { asFragment, getByText } = render(
+      <Card>This is the content of the card.</Card>
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+    expect(getByText(/This is the content of the card/)).toBeInTheDocument();
+    expect(getByText(/This is the content of the card/))
+      .toHaveStyle("user-select: none");
+  });
+
+  test("Non Selectable", () => {
+    const { asFragment, getByText } = render(
+      <Card selectable>This is the content of the card.</Card>
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+    expect(getByText(/This is the content of the card/)).toBeInTheDocument();
+    expect(getByText(/This is the content of the card/))
+      .toHaveStyle("user-select: text");
+  });
+});
