80210f3330500e64428675358a8803a0cad58617
Sergio Xalambrí <hello@sergiodxa.com>
2019-09-16 09:55:51 -0300

    Fix Prettier on Header tests

--- src/components/header.test.tsx
+++ src/components/header.test.tsx
@@ -1,5 +1,5 @@
 /** * @jest-environment jsdom */
-import '@testing-library/jest-dom/extend-expect'
+import "@testing-library/jest-dom/extend-expect";
 import { jsx } from "@emotion/core";
 import { render } from "@testing-library/react";
 import Header from "./header";
@@ -28,14 +28,16 @@ const baseState = {
       links: "Links",
       slides: "Talks",
       resume: "CV"
-    },
+    }
   }
-}
+};
 
 describe("Header", () => {
   test("Empty", () => {
     const { asFragment } = render(
-      <StateProvider state={baseState}><Header /></StateProvider>
+      <StateProvider state={baseState}>
+        <Header />
+      </StateProvider>
     );
 
     expect(asFragment()).toMatchSnapshot();
@@ -55,8 +57,10 @@ describe("Header", () => {
 
     expect(asFragment()).toMatchSnapshot();
     expect(getByText(/Contentz/)).toBeInTheDocument();
-    expect(getByText(/Contentz/))
-      .toHaveAttribute("title", baseState.i18n.header.mainLinkDescription);
+    expect(getByText(/Contentz/)).toHaveAttribute(
+      "title",
+      baseState.i18n.header.mainLinkDescription
+    );
     expect(getByText(/Contentz/)).toHaveAttribute("href", "/");
   });
 
@@ -95,34 +99,52 @@ describe("Header", () => {
 
     // Title
     expect(getByText(/Contentz/)).toBeInTheDocument();
-    expect(getByText(/Contentz/))
-      .toHaveAttribute("title", baseState.i18n.header.mainLinkDescription);
+    expect(getByText(/Contentz/)).toHaveAttribute(
+      "title",
+      baseState.i18n.header.mainLinkDescription
+    );
     expect(getByText(/Contentz/)).toHaveAttribute("href", "/");
 
     // Content
     expect(getByText(baseState.i18n.header.articles)).toBeInTheDocument();
-    expect(getByText(baseState.i18n.header.articles))
-      .toHaveAttribute("target", "_self");
-    expect(getByText(baseState.i18n.header.articles))
-      .toHaveAttribute("href", "/articles/");
-    
-      expect(getByText(baseState.i18n.header.links)).toBeInTheDocument();
-    expect(getByText(baseState.i18n.header.links))
-      .toHaveAttribute("target", "_self");
-    expect(getByText(baseState.i18n.header.links))
-      .toHaveAttribute("href", "/links/");
-    
-      expect(getByText(baseState.i18n.header.slides)).toBeInTheDocument();
-    expect(getByText(baseState.i18n.header.slides))
-      .toHaveAttribute("target", "_self");
-    expect(getByText(baseState.i18n.header.slides))
-      .toHaveAttribute("href", "/slides/");
-    
-      expect(getByText(baseState.i18n.header.resume)).toBeInTheDocument();
-    expect(getByText(baseState.i18n.header.resume))
-      .toHaveAttribute("target", "_self");
-    expect(getByText(baseState.i18n.header.resume))
-      .toHaveAttribute("href", "/cv/");
+    expect(getByText(baseState.i18n.header.articles)).toHaveAttribute(
+      "target",
+      "_self"
+    );
+    expect(getByText(baseState.i18n.header.articles)).toHaveAttribute(
+      "href",
+      "/articles/"
+    );
+
+    expect(getByText(baseState.i18n.header.links)).toBeInTheDocument();
+    expect(getByText(baseState.i18n.header.links)).toHaveAttribute(
+      "target",
+      "_self"
+    );
+    expect(getByText(baseState.i18n.header.links)).toHaveAttribute(
+      "href",
+      "/links/"
+    );
+
+    expect(getByText(baseState.i18n.header.slides)).toBeInTheDocument();
+    expect(getByText(baseState.i18n.header.slides)).toHaveAttribute(
+      "target",
+      "_self"
+    );
+    expect(getByText(baseState.i18n.header.slides)).toHaveAttribute(
+      "href",
+      "/slides/"
+    );
+
+    expect(getByText(baseState.i18n.header.resume)).toBeInTheDocument();
+    expect(getByText(baseState.i18n.header.resume)).toHaveAttribute(
+      "target",
+      "_self"
+    );
+    expect(getByText(baseState.i18n.header.resume)).toHaveAttribute(
+      "href",
+      "/cv/"
+    );
 
     // Custom Navigation
     expect(getByText(/About/)).toBeInTheDocument();
@@ -130,9 +152,11 @@ describe("Header", () => {
     expect(getByText(/About/)).toHaveAttribute("target", "_self");
 
     expect(getByText(/Books/)).toBeInTheDocument();
-    expect(getByText(/Books/))
-      .toHaveAttribute("href", "https://leanpub.com/u/sergiodxa/");
+    expect(getByText(/Books/)).toHaveAttribute(
+      "href",
+      "https://leanpub.com/u/sergiodxa/"
+    );
     expect(getByText(/Books/)).toHaveAttribute("rel", "nofollow noopener");
     expect(getByText(/Books/)).toHaveAttribute("target", "_blank");
-  })
+  });
 });
